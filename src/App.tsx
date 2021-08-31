4fcd3f552aa6a6130c825a739a67c837cdebb89b
gi-carnaval <gi.carnaval@hotmail.com>
2021-08-31 00:43:21 -0300

    31/08/2021
    Criação do Modal de Novas Transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { TransactionsTable } from "./components/TransactionsTable";
+//import { TransactionsTable } from "./components/TransactionsTable";
+import { NewTransactionModal } from "./components/NewTransactionsModal";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
-      <TransactionsTable />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
 }
-
-
